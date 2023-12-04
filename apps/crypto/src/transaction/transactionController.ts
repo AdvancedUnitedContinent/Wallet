05475e34fa7485d591ca357bb1bb2433fd08f26b
SahSha <yjs@ennovalabs.com>
2023-12-04 17:16:59 +0900

    Add Transaction API: create send

---
+++ apps/crypto/src/transaction/transactionController.ts
@@ -0,0 +1,10 @@
+export class TransactionController {
+    constructor(private readonly transactionService: TransactionService) {}
+    @ApiOperation({summary: '송금 요청'})
+    @ApiBody({type: SendCreateReq})
+    @Post('send')
+    async createSend(@Req() req, @Body() sendCreateReq: SendCreateReq): Promise<CustomHttpResponse> {
+        sendCreateReq.sender = req.user.memberNo;
+        return await this.transactionService.createSend(sendCreateReq);
+    }
+}
