b88a13a184e26995ea5243f80bcc1ab181450040
SahSha <yjs@ennovalabs.com>
2023-12-28 06:02:47 +0900

    Add Transaction API: get swap rate

--- apps/crypto/src/cryptoController.ts
+++ apps/crypto/src/cryptoController.ts
@@ -8,4 +8,16 @@ export class CryptoController {
     async createCrypto(@Body() cryptoCreateReq: CryptoCreateReq): Promise<CustomHttpResponse> {
         return await this.cryptoService.createCrypto(cryptoCreateReq);
     }
+
+    @ApiOperation({summary: '전환 비율 조회'})
+    @ApiQuery({name: 'fromSymbol', description: '코인 심볼'})
+    @ApiQuery({name: 'toSymbol', description: '전환 심볼'})
+    @Get('swap/price')
+    async getSwapPrice(
+        @Query('fromSymbol') fromSymbol: string,
+        @Query('toSymbol') toSymbol: string
+    ): Promise<CustomHttpResponse> {
+        return await this.cryptoService.getSwapPrice(fromSymbol, toSymbol);
+    }
+
 }
