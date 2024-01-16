7a149f1589f06b1caec89af5352d1f6c91b5bc81
SahSha <yjs@ennovalabs.com>
2024-01-16 23:11:18 +0900

    Add Histoet API: transaction list

---
+++ apps/crypto/src/history/historyController.ts
@@ -0,0 +1,26 @@
+export class HistoryController {
+    constructor(private readonly historyService: HistoryService) {}
+
+    @ApiOperation({ summary: '거래 내역 조회' })
+    @ApiQuery({ name: 'coinSymbol', description: '코인 심볼', required: false })
+    @ApiQuery({ name: 'txType', description: '거래 타입', required: false })
+    @ApiQuery({ name: 'startDate', description: '조회 시작일', required: false })
+    @ApiQuery({ name: 'endDate', description: '조회 종료일', required: false })
+    @ApiQuery({ name: 'pageNo', description: '페이지 번호', required: false })
+    @ApiQuery({ name: 'pageSize', description: '페이지 크기', required: false })
+    @ApiQuery({ name: 'sortType', description: '정렬 순서', required: false })
+    @Get('')
+    async getTransaction(
+        @Req() req,
+        @Query('coinSymbol') coinSymbol?: string,
+        @Query('txType') txType?: string,
+        @Query('startDate') startDate?: string,
+        @Query('endDate') endDate?: string,
+        @Query('pageNo') pageNo?: number,
+        @Query('pageSize') pageSize?: number,
+        @Query('sortType') sortType?: string,
+    ): Promise<CustomHttpResponse> {
+        const memberNo: number = req.user.memberNo;
+        return await this.historyService.getTransaction(memberNo, coinSymbol, txType, startDate, endDate, pageNo, pageSize, sortType);
+    }
+}
