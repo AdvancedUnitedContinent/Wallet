c1d7c30c66f939bba6c92bdcc8ea1b81df03baef
Song HyeRin <hrs0518@ennovalabs.com>
2024-03-10 22:23:30 +0900

    Add Crypto API: Edit Withdrawal wallet address

--- apps/crypto/src/cryptoController.ts
+++ apps/crypto/src/cryptoController.ts
@@ -74,7 +74,18 @@ export class CryptoController {
         );
     }
 
-<<<<<<< HEAD
+    @ApiOperation({ summary: '출금 주소록 수정' })
+    @Put('withdrawal/addressbook')
+    async updateWalletAddressBook(
+        @Req() req,
+        @Body() updWalletAddressBookReq: UpdWalletAddressBookReq,
+    ): Promise<CustomHttpResponse> {
+        updWalletAddressBookReq.memberNo = req.user.memberNo;
+        return await this.cryptoService.updateWalletAddressBook(
+            updWalletAddressBookReq,
+        );
+    }
+    
     @ApiOperation({ summary: '출금 주소록 수정' })
     @Put('withdrawal/addressbook')
     async updateWalletAddressBook(
@@ -100,9 +111,7 @@ export class CryptoController {
             memberNo,
         );
     }
-
-=======
->>>>>>> 73ffb19 (Add Crypto API: Withdrawal wallet address register)
+    
     @ApiOperation({summary: '약관 조회'})
     @Get('terms')
     async getTerms(): Promise<CustomHttpResponse> {
