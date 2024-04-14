190d7ecf18fb5f7176ce1d3814a5c8e707f9051f
Song HyeRin <hrs0518@ennovalabs.com>
2024-04-15 01:13:40 +0900

    Add History API: Get User Wallet By Crypto Type

--- apps/crypto/src/wallet/walletController.ts
+++ apps/crypto/src/wallet/walletController.ts
@@ -51,4 +51,13 @@ export class WalletController {
     ): Promise<CustomHttpResponse> {
         return await this.walletService.getMemberWalletList(coinSymbol);
     }
+
+    @ApiOperation({summary: '코인 타입 별 회원 지갑 목록 조회'})
+    @ApiQuery({name: 'cryptoType', description: '코인 타입'})
+    @Get('/member/cryptoType')
+    async getMemberWalletListByCryptoType(
+        @Query('cryptoType') cryptoType: string
+    ): Promise<CustomHttpResponse> {
+        return await this.walletService.getMemberWalletListByCryptoType(cryptoType);
+    }
 }
